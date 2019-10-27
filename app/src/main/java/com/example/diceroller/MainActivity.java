2b312cf92bb05a6b218b0003c2d5f7baaa23c03a
chris <chris.rc1@hotmail.com>
2019-10-27 22:38:21 +0000

    I've completed part 1 and part 3a - Christopher Rojas 181670

--- app/src/main/java/com/example/diceroller/MainActivity.java
+++ app/src/main/java/com/example/diceroller/MainActivity.java
@@ -11,11 +11,15 @@ import androidx.appcompat.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import java.util.ArrayList;
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -58,13 +62,67 @@ public class MainActivity extends AppCompatActivity {
 
 public void on_button_click(View view){
 
-        TextView tv = this.findViewById(R.id.textView);
+        roll_dice();
 
-        Random r = new Random();
-        int number = r.nextInt(6);
+        TextView tv = (TextView) this.findViewById(R.id.textView);
+        EditText tv2 = (EditText) this.findViewById(R.id.editText2);
+        TextView tv3 = (TextView) this.findViewById(R.id.textView3);
 
-        tv.setText(Integer.toString(number));
+
+       // Random r = new Random();
+       // int max=6, min=1;
+       // int number = r.nextInt((max - min) + 1) + min;
+
+       // tv.setText(Integer.toString(number));
+
+        int first = Integer.valueOf(tv.getText().toString());
+        int second = Integer.valueOf(tv2.getText().toString());
+
+        if ( first == second) {
+            int third = Integer.valueOf(tv3.getText().toString());
+            int scoreUp = third + 1;
+            tv3.setText(Integer.toString(scoreUp));
+
+            Toast.makeText(getApplicationContext(),"Congratulations", Toast.LENGTH_SHORT).show();
+
+        }
+
 
    }
+   public void roll_dice ()
+   {
+       TextView tv = (TextView) this.findViewById(R.id.textView);
+
+       Random r = new Random();
+       int max=6, min=1;
+       int number = r.nextInt((max - min) + 1) + min;
+
+       tv.setText(Integer.toString(number));
+
+
+
+   }
+public void iceBreakers (View view)
+{
+    roll_dice();
+
+    TextView tv = (TextView) this.findViewById(R.id.textView);
+    int number = Integer.valueOf(tv.getText().toString());
+
+    ArrayList <String> arrayQuestions = new ArrayList();
+    arrayQuestions.add("If you could go anywhere in the world, where would you go?");
+    arrayQuestions.add("If you were stranded on a desert island, what three things would you want to take with you?");
+    arrayQuestions.add("If you could eat only one food for the rest of your life, what would that be?");
+    arrayQuestions.add("If you won a million dollars, what is the first thing you would buy?");
+    arrayQuestions.add("If you could spaned the day with one fictional character, who would it be?");
+    arrayQuestions.add("If you found a magic lantern and a genie gave you three wishes, what would you wish?");
+
+    TextView questionBox = (TextView) this.findViewById(R.id.questionBox);
+    questionBox.setText(arrayQuestions.get(number));
 
 }
+
+}
+
+
+
